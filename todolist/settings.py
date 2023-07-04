37bb6f2ffe0b356e0a545ef9f378fe8ad373547c
ArtemShadrin <artemshadrinprogramming@gmail.com>
2023-07-04 13:59:18 +0700

    add model user add migrations add admin

--- todolist/settings.py
+++ todolist/settings.py
@@ -80,8 +80,13 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str('DB_NAME'),
+        "USER": env.str('DB_USER'),
+        "PASSWORD": env.str('DB_PASSWORD'),
+        "HOST": env.str('DB_HOST', default='127.0.0.1'),
+        "PORT": "5432",
+        "DATABASE_URL": env.str('DB_URL')
     }
 }
 
@@ -126,3 +131,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'core.User'
